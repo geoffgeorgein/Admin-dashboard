77157819eb17066a185f85efe6013214a5e928dd
Geoff George <geoffgeorgethommana@gmail.com>
2023-04-13 20:29:37 +0530

    singleContainer

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Single.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
 function single() {
   return (
-    <div>single</div>
+    <div className='single'>
+
+      <Sidebar/>
+      <div className='singleContainer'>
+        <Navbar/>
+        <div className='top'>
+          <div className='left'>
+
+            <h1 className='title'>Information</h1>
+            <div className='item'>
+
+            <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt=""
+                className="itemImg"
+              />
+            </div>
+          </div>
+          <div className='right'>
+
+          </div>
+        </div>
+
+        <div className='bottom'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
